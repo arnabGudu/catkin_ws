9851212f2197a60ed0c50dc67891433143283190
Lucifer <arnabgudu.gs@gmail.com>
2020-04-09 19:28:08 +0530

    added

--- src/lidar/src/lidar.py
+++ src/lidar/src/lidar.py
@@ -2,18 +2,67 @@ import rospy
 import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import PointCloud2, LaserScan
 from laser_geometry.laser_geometry import LaserProjection
+import numpy as np
 
 lp = LaserProjection()
 pub = rospy.Publisher("cloud", PointCloud2, queue_size=100)
 
+def create_blocks(R, start, stop):
+	j = 0
+	for i in range(len(R)):
+		if R[i] != np.inf:
+			j = j + 1
+		if R[i - 1] == np.inf and R[i] != np.inf:
+			start.append(j)
+		if R[i - 1] != np.inf and R[i] == np.inf:
+			stop.append(j-1)
+
+def break_blocks(p, start, stop, width = 1):
+	for i in range(len(p)):
+		d = np.sqrt((p[i-1][0] - p[i][0]) ** 2 +
+					(p[i-1][1] - p[i][1]) ** 2	)
+		if (d > width):
+			stop.append(p[i-1][4])
+			stop.sort()
+			start.append(p[i][4])
+			start.sort()
+
+# def merge_blocks(p, start, stop, width = 1):
+# 	if (start[0] < stop[0]):
+# 		for i in range(len(start)):
+# 			d = np.sqrt((p[i-1][0] - p[i][0]) ** 2 +
+# 					(p[i-1][1] - p[i][1]) ** 2	)
+# 	else:
+# 		for i in range(len(stop)):
+# 			d = np.
+
+def main_function(R, P):
+	start = []
+	stop = []
+	create_blocks(R, start, stop)
+	# print_data(R, start, stop)
+	# print("***************************")
+	# break_blocks(P, start, stop)
+	# merge_blocks()
+	print_data(R, start, stop)
+	print_data(P, start, stop)
+
+def print_data(data, start, stop):
+	for i in range(len(data)):
+		print(i, data[i])
+	print("###########################")
+	print(start, stop)
+	print(len(start), len(stop))
+	print("///////////////////////////")
+
 def callback(data):
+	R = data.ranges
 	cloud = lp.projectLaser(data)
 	pub.publish(cloud)
 	point_gen = pc2.read_points(cloud)
-	
-	for i, points in enumerate(point_gen):
-		print(i, points)
-	
+	P = list(point_gen)
+	main_function(R, P)
+
 def listener():
 	rospy.init_node('lidar', anonymous=True)
 
--- src/lidar/src/lidar_class.py
+++ src/lidar/src/lidar_class.py
@@ -1,4 +1,5 @@
 import rospy
+import numpy as np
 import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import PointCloud2, LaserScan
 from laser_geometry.laser_geometry import LaserProjection
@@ -7,31 +8,34 @@ class lidar:
 	def __init__(self):
 		rospy.init_node('lidar', anonymous=True)
 		rospy.Subscriber("scan", LaserScan, self.callback)
-		
+
 		self.lp = LaserProjection()
-		self.pub = rospy.Publisher("cloud", PointCloud2, 
-									queue_size=100)	
+		self.pub = rospy.Publisher("cloud", PointCloud2,
+									queue_size=100)
 		self.queue = []
 		rospy.spin()
-		
+
 	def callback(self, data):
 		#self.median_filter(data)
-		
-		cloud = self.lp.projectLaser(data)
-		self.pub.publish(cloud)
-		
-		self.point_gen = pc2.read_points(cloud)
-		self.Print()
-	
+		self.R = data.ranges							#raw laserscan data in form-
+												#of R(theta)
+		cloud = self.lp.projectLaser(data)		#convert laserscan to pc2
+		self.pub.publish(cloud)					#publish pc2 to /cloud topic
+
+		self.point_gen = pc2.read_points(cloud)	#point_gen reads pc2 data
+		self.Print()							#prints pc2 data
+
+		#self.preprocessing()
+
 	def Print(self):
 		for i, points in enumerate(self.point_gen):
 			print(i, points)
-		
+
 	def median_filter(self, laserScan):
 		self.queue.append(laserScan.ranges)
 		if len(self.queue) > 3:
 			self.queue.pop(0)
-		elif len(self.queue) == 3:	
+		elif len(self.queue) == 3:
 			median = []
 
 			for i in range(360 - 2):
@@ -40,10 +44,22 @@ class lidar:
 						self.queue[2][i], self.queue[2][i+1], self.queue[2][i+2]]
 				mat.sort()
 				median.append(mat[9 / 2])
-		
+
 			print (median)
-			
-			
+
+	#def preprocessing(self):
+
+		#for i in range(len(self.R) - 1):
+			#if (self.R[i-1] == np.inf and self.R[i] != np.inf):
+
+			#if (self.R[i] != np.inf and self.R[i+1] == np.inf):
+
+
+		print("#######################################")
+		print(self.R)
+		print("#######################################")
+		print(blocks)
+
 
 if __name__ == '__main__':
 	l = lidar()
