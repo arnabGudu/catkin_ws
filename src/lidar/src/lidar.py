905d4c0a62c049e2ec7e1683476149c212322eb6
arnabGudu <arnabgudu.gs@gmail.com>
2020-04-13 15:12:41 +0530

    added

--- src/lidar/src/lidar.py
+++ src/lidar/src/lidar.py
@@ -1,6 +1,8 @@
 import rospy
-from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan, PointCloud, ChannelFloat32
 import numpy as np
+import std_msgs.msg
+from geometry_msgs.msg import Point32
 
 def create_blocks(R, start, stop):
 	for i in range(len(R)):
@@ -15,7 +17,6 @@ def create_blocks(R, start, stop):
 
 def break_blocks(p, start, stop, width = 1):
 	for n,s in enumerate(start):
-
 		for i in range(s, stop[n]):
 			print(i, start[n], stop[n])
 			d = np.sqrt((p[i][0] - p[i+1][0]) ** 2 +
@@ -26,7 +27,7 @@ def break_blocks(p, start, stop, width = 1):
 				print("............")
 	start.sort()
 	stop.sort()
-	print(start, stop)
+	print(start, stop, len(start))
 
 def merge_blocks(p, start, stop, width = 1):
 	for i, s in enumerate(start):
@@ -37,23 +38,55 @@ def merge_blocks(p, start, stop, width = 1):
 			start.pop(i)
 			stop.pop(i-1)
 
-def clustering(p, start, stop):
+def clustering(P, start, stop):
 	for i in range(len(start)):
-		x = p[stop[i]] - p[start[i]]
-		if (x < 5):
-			O = p[]
+		print("...............")
+		X = P[start[i] : stop[i]]
+		n = len(X)
+		if (n < 5 and n > 0):
+			O = (X[0] + X[-1]) / 2
+			d = np.sqrt(np.sum( (X - O) ** 2, axis = 0))
+		 	D = np.max(d)
+		else:
+			a = (X[-1][1] - X[0][1]) / (X[-1][0] - X[0][0])
+			b = -a * X[0][0] + X[0][1]
+			d = (a*X + b) / np.sqrt(a ** 2 + b ** 2)
+			s = np.sqrt(np.sum((X[-1] - X[0]) ** 2, axis = 0))
+			D = np.max(np.abs(d))
+			if D < 0.2 * s:
+				cluster.append((X[0], X[-1]))
+			else:
+				b_above = np.max(d) * np.sqrt(a ** 2 + b ** 2)
+				b_below = np.min(d) * np.sqrt(a ** 2 + b ** 2)
 
 def main_function(R, P):
 	start = []
 	stop = []
 	create_blocks(R, start, stop)
-	print_data(R, start, stop)
+	# print_data(P, start, stop)
 	break_blocks(P, start, stop)
-	print(start, stop)
-	print(len(start), len(stop))
+	# print(start, stop)
+	# print(len(start), len(stop))
 	# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 	# merge_blocks(P, start, stop)
 	# print_data(R, start, stop)
+	# clustering(P, start, stop)
+
+def publish_pointCloud(P):
+	pc = PointCloud()
+	pub = rospy.Publisher("cloud2", PointCloud, queue_size=100)
+	header = std_msgs.msg.Header()
+	header.stamp = rospy.Time.now()
+	header.frame_id = 'laser'
+	pc.header = header
+	for p in P:
+		if (p[0] != np.inf):
+			pc.points.append(Point32(p[0],p[1],0))
+	ch_rgb = ChannelFloat32()
+	ch_rgb.name = "rgb"
+	ch_rgb.values = (255,255,255)
+	pc.channels.append(ch_rgb)
+	pub.publish(pc)
 
 def print_data(data, start, stop):
 	for i in range(len(data)):
@@ -70,7 +103,9 @@ def callback(data):
 		x = r * np.cos(i * np.pi / 180 - 3.12413907051)
 		y = r * np.sin(i * np.pi / 180 - 3.12413907051)
 		P.append((x, y))
-	main_function(R, P)
+	P = np.array(P)
+	publish_pointCloud(P)
+	# main_function(R, P)
 
 def listener():
 	rospy.init_node('lidar', anonymous=True)
